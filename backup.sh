3b0ebf6546196032ebfd8ce5ac6baab945a23558
dighist AWS EC2 instance <digitalhistor@fas.harvard.edu>
2016-05-20 16:07:48 +0000

    read db.ini from dir, use to backup database
    fixes #1. Reading from ini could probably be cleaner, but it works.

--- backup.sh
+++ backup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo 'config file: ' $1
 source $1
-echo 'db name: ' $db_name
+db_user=$(awk -F "=" '/username/ {print $2}' $dir_to_backup/db.ini)
+db_user="${db_user%\"}"
+db_user="${db_user# \"}"
+db_pwd=$(awk -F "=" '/password/ {print $2}' $dir_to_backup/db.ini)
+db_pwd="${db_pwd%\"}"
+db_pwd="${db_pwd# \"}"
+db_name=$(awk -F "=" '/dbname/ {print $2}' $dir_to_backup/db.ini)
+db_name="${db_name%\"}"
+db_name="${db_name# \"}"
 mkdir -p _backups/$save_dir
-mysqldump -u root -p $db_name > _backups/$save_dir/db_backup.sql
+mysqldump -u $db_user -p$db_pwd $db_name > _backups/$save_dir/db_backup.sql
 zip -r _backups/$save_dir/directory.zip $dir_to_backup 
